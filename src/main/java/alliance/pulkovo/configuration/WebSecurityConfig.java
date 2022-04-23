50a2716f4aa01df041e933242ca127912d4e3bd6
Grigory Yakovlev <71254983+ykvlv@users.noreply.github.com>
2022-04-23 11:03:11 +0300

    templates added

--- src/main/java/alliance/pulkovo/configuration/WebSecurityConfig.java
+++ src/main/java/alliance/pulkovo/configuration/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package alliance.pulkovo;
+package alliance.pulkovo.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
